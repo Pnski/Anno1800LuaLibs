69a67228a9f95942a1d929982af87720c84da02e
pnski <s1l3nce@hotmail.de>
2024-02-16 18:51:18 +0100

    u

---
+++ 1800seralize.lua
@@ -0,0 +1,94 @@
+-- https://www.reddit.com/r/lua/comments/hzi7ff/print_local_variable_as_hex_string/
+local function HexToString(hex)
+  return (hex:gsub("%x%x", function(digits) return string.char(tonumber(digits, 16)) end))
+end
+
+-- https://www.reddit.com/r/lua/comments/hzi7ff/print_local_variable_as_hex_string/
+--string.format(string.rep("%02x", #variable), variable:byte(1, -1)))
+local function StringToHex(str)
+  return (str:gsub(".", function(char) return string.format("%02x", char:byte()) end))
+end
+-- Read as: substitute every character in variable with its byte value formatted as hexadecimal zero-padded to at least 2 digits
+
+--[[old code
+  Check given table for metatabledata
+
+local function check_mt(_table)
+  if _table.__index ~= nil then
+    return true -- is a metatable
+  else
+    return false --is NOT a metatable
+  end
+end]]
+
+--[[
+  returns table with pairs
+]]
+local function getkvtable(_table)
+  if _table.__index ~= nil then
+    return _table
+  else
+    return debug.getmetatable(_table) --using debug library, instead of getmetatable(table:table)
+  end
+  --[[if check_mt(_table) == true then
+    return debug.getmetatable(_table)
+  else
+    return _table
+  end]]
+end
+
+--There are eight basic types in Lua: nil, boolean, number, string, userdata, function, thread, and table. 
+--[[
+  user giving functions or threads is a stupid idea
+  userdata is normally from c, no need to test
+]]
+local function ValueToString(_value)
+  if type(_value) == 'nil' then
+    return "nil"
+  elseif type(_value) == 'number' then
+    return _value
+  elseif type(_value) == 'string' then
+    return 
+  elseif type(_value) == 'boolean' then
+  else --failsafe
+    print("plz check your code bro")
+    return
+  end
+end
+
+--   table will be looped, no need to test, rest as above
+local function IndexToString(_index)
+  if type(_index) == 'nil' then
+    return "nil"
+  elseif type(_index) == 'number' then
+    return "[".._index.."]"
+  elseif type(_index) == 'string' then
+    return "[\"".._index.."\"]"
+  elseif type(_index) == 'boolean' then
+    if _index then --easier to check and return the boolean number back than true/false which might be a string
+      return 1
+    else
+      return 0
+    end
+  elseif type(_index) == 'table' then
+  else --failsafe
+    print("plz check your code bro")
+    return
+  end
+end
+
+local function TableToString(_table)
+  local _string = ""
+  if type(_table) ~= 'table' then
+    return
+  end
+  -- convert every array or metatable to array (pairs might fail if C-API call is blocked)
+  for k,v in pairs(getkvtable(_table)) do
+    if type(_table[k]) == 'table' then
+      getDeepTable(_table[k], indent + 1)
+    else
+      _string = _string..ValueToString(k).."="..ValueToString(v)..","
+    end
+  end
+  return _string
+end
--- a1800io.lua
+++ a1800io.lua
@@ -1,10 +1,30 @@
 -- io related stuff
 require 'io'
+--require 'a1800info'
 
-local function curl(URL)
-    io.popen("curl "..URL)
+local function curl(URL,_path)
+    io.popen("curl "..URL.." > ".. _path)
 end
 
 local function openFile(_path)
     return io.open(_path, "a")
 end
+
+--returning the default path
+-- [defaultAnnoPath]\ModSaveFiles\[CompanyName]\[ModName].csv
+local function dCSVPath(ModName)
+    local _path = getCurrentCompanyName()
+end
+
+-- todo kill all nonfunctional characters
+-- mkdir [Anno 1800]/"your\\path\\to\\folders"/[file]
+local function mkdir(_path)
+    local _path_mod = _path:gsub("%p%a*%..*","") -- cut file
+    _path_mod = _path_mod:gsub("/","\\\\") --= \\ due to code limitations --replace all / with \\
+    local file = io.popen("mkdir ".._path_mod)
+    file:close() -- free buffer
+end
+
+aLibs = {
+    mkdir = mkdir
+}
