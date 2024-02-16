f11f649ec6c4e9a992cfdb7e71bf16bf06b86395
pnski <s1l3nce@hotmail.de>
2024-02-16 20:14:04 +0100

    u_ testing

--- 1800seralize.lua
+++ 1800seralize.lua
@@ -10,35 +10,20 @@ local function StringToHex(str)
 end
 -- Read as: substitute every character in variable with its byte value formatted as hexadecimal zero-padded to at least 2 digits
 
---[[old code
-  Check given table for metatabledata
-
-local function check_mt(_table)
-  if _table.__index ~= nil then
-    return true -- is a metatable
-  else
-    return false --is NOT a metatable
-  end
-end]]
-
 --[[
   returns table with pairs
 ]]
 local function getkvtable(_table)
-  if _table.__index ~= nil then
+  if _table.__index == nil then
     return _table
   else
     return debug.getmetatable(_table) --using debug library, instead of getmetatable(table:table)
   end
-  --[[if check_mt(_table) == true then
-    return debug.getmetatable(_table)
-  else
-    return _table
-  end]]
 end
 
 --There are eight basic types in Lua: nil, boolean, number, string, userdata, function, thread, and table. 
 --[[
+  table will be looped, no need to test
   user giving functions or threads is a stupid idea
   userdata is normally from c, no need to test
 ]]
@@ -48,29 +33,24 @@ local function ValueToString(_value)
   elseif type(_value) == 'number' then
     return _value
   elseif type(_value) == 'string' then
-    return 
+    return "\"".._value.."\""
   elseif type(_value) == 'boolean' then
+    if _value then --easier to check and return the boolean number back than true/false which might be a string
+      return 1
+    else
+      return 0
+    end
   else --failsafe
     print("plz check your code bro")
     return
   end
 end
 
---   table will be looped, no need to test, rest as above
 local function IndexToString(_index)
-  if type(_index) == 'nil' then
-    return "nil"
-  elseif type(_index) == 'number' then
+  if type(_index) == 'number' then
     return "[".._index.."]"
   elseif type(_index) == 'string' then
     return "[\"".._index.."\"]"
-  elseif type(_index) == 'boolean' then
-    if _index then --easier to check and return the boolean number back than true/false which might be a string
-      return 1
-    else
-      return 0
-    end
-  elseif type(_index) == 'table' then
   else --failsafe
     print("plz check your code bro")
     return
@@ -78,21 +58,33 @@ local function IndexToString(_index)
 end
 
 local function TableToString(_table)
-  local _string = ""
+  local _string = "{"
   if type(_table) ~= 'table' then
     return
   end
   -- convert every array or metatable to array (pairs might fail if C-API call is blocked)
   for k,v in pairs(getkvtable(_table)) do --getkvtable to cycle through
+    if #_string > 1 then
+      _string = _string ..","
+    end
     if type(_table[k]) == 'table' then -- use the original table for direct values
-      _string = _string..TableToString(_table[k])
+      _string = _string..IndexToString(k).."="..TableToString(_table[k])
     else
-      _string = _string..IndexToString(k).."="..ValueToString(v)..","
+      _string = _string..IndexToString(k).."="..ValueToString(v)
     end
   end
-  return _string
+  return _string.."}"
 end
 
-local function StringToTable(_string)
-  return loadstring(_string)
+local function TableToHex(_table)
+  return StringToHex(TableToString(_table))
 end
+
+local function HexToTable(_string)
+  return loadstring(HexToString(_string))
+end
+
+a1800 = {
+  HexToTable = HexToTable,
+  TableToHex = TableToHex
+}
