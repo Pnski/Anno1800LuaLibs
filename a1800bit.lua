80676a2ea879ff14c87dae73edf1aabcc37d629a
pnski <s1l3nce@hotmail.de>
2024-02-18 22:36:58 +0100

    u_ bugs

--- a1800bit.lua
+++ a1800bit.lua
@@ -1,26 +1,24 @@
-local function AreaToID(SessionID,IslandID,AreaIndex)
+local function AreaToID(AreaIndex,IslandID,SessionID)
     if SessionID == nil or IslandID == nil or AreaIndex == nil then
         print("Error in AreaToID returning 0")
         return 0
     end
-    return ((SessionID << 13)+(IslandID << 6)+AreaIndex)
+    return ((AreaIndex << 13)+(IslandID << 6)+SessionID)
 end
 
 local function IDToArea(AreaID)
-    local SessionID = ((AreaID & 0xE000) >> 13)
+    local AreaIndex = ((AreaID & 0xE000) >> 13)
     local IslandID = ((AreaID & 0x1FC0) >> 6)
-    local AreaIndex = (AreaID & 0xF)
-    return SessionID, IslandID, AreaIndex
+    local SessionID = (AreaID & 0xF)
+    return AreaIndex, IslandID, SessionID
 end
 
 local function toOID(...)
     local arg = {...}
     if #arg == 2 then --ObjectID,AreaID
-        local SessionID,IslandID,AreaIndex = IDToArea(arg[2])
-        print(SessionID,IslandID,AreaIndex, arg[2],IDToArea(arg[2]))
-        return ((SessionID << 32) + (arg[1]))
-    elseif #arg == 4 then --ObjectID,SessionID,IslandID,AreaIndex
         return ((arg[2] << 32) + (arg[1]))
+    elseif #arg == 4 then --ObjectID,AreaIndex,IslandID,SessionID
+        return (((AreaToID(arg[2],arg[3],arg[4])) << 32) + (arg[1]))
     else
         print("error in number of args")
         return 0
@@ -38,3 +36,4 @@ a1800 = {
 --print(IDToArea(8514))
 --print(toOID(32,8514))
 --print(toOID(32,2,5,2))
+--print(toOID(1,1,6,2))
