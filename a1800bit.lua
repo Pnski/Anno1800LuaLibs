f51e047361c5f6811aabbe0773d42f5e7c1ddf30
pnski <s1l3nce@hotmail.de>
2024-02-18 22:14:24 +0100

    u_ oid in bit

--- a1800bit.lua
+++ a1800bit.lua
@@ -1,31 +1,40 @@
---[[for n = 1,2 do
-    for m = 5,26 do
-      print((n << 13)+(m << 6)+2)
-    end
-  end
-  _test =8514
-  local t= {}
-    t[1] = ((_test & 57344)>>13)
-    t[2] = ((_test & 8128)>>6)
-    t[3] = (_test & 15)
-  print(t[1],t[2],t[3])
-]]
 local function AreaToID(SessionID,IslandID,AreaIndex)
+    if SessionID == nil or IslandID == nil or AreaIndex == nil then
+        print("Error in AreaToID returning 0")
+        return 0
+    end
     return ((SessionID << 13)+(IslandID << 6)+AreaIndex)
 end
 
 local function IDToArea(AreaID)
-    local SessionID = ((AreaID & 57344)>>13)
-    local IslandID = ((AreaID & 8128)>>6)
-    local AreaIndex = (AreaID & 15)
+    local SessionID = ((AreaID & 0xE000) >> 13)
+    local IslandID = ((AreaID & 0x1FC0) >> 6)
+    local AreaIndex = (AreaID & 0xF)
     return SessionID, IslandID, AreaIndex
+end
+
+local function toOID(...)
+    local arg = {...}
+    if #arg == 2 then --ObjectID,AreaID
+        local SessionID,IslandID,AreaIndex = IDToArea(arg[2])
+        print(SessionID,IslandID,AreaIndex, arg[2],IDToArea(arg[2]))
+        return ((SessionID << 32) + (arg[1]))
+    elseif #arg == 4 then --ObjectID,SessionID,IslandID,AreaIndex
+        return ((arg[2] << 32) + (arg[1]))
+    else
+        print("error in number of args")
+        return 0
+    end
 end
 
 a1800 = {
     convertAIDtoArea = IDToArea,
-    convertAreaToAID = AreaToID
+    convertAreaToAID = AreaToID,
+    convertToOID = toOID
 }
 
 --test
---print(AreaToID(1,5,2))
---print(IDToArea(8514))
+--print(AreaToID(1,nil,1))
+print(IDToArea(8514))
+print(toOID(32,8514))
+print(toOID(32,2,5,2))
